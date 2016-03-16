2a866854bb9ed64403d5fc2d0f31193d3e3c58c7
AdrianoDee <adriano.diflorio@ba.infn.it>
2016-03-16 20:18:50 +0100

    index_int

--- PDFs/IndexSelectorPdf.cu
+++ PDFs/IndexSelectorPdf.cu
@@ -5,19 +5,24 @@ __device__ fptype device_IndexSelector (fptype* evt, fptype* p, unsigned int* in
   fptype x = evt[indices[2 + indices[0]]];
   unsigned int index_int = FLOOR(0.5 + x);
   unsigned int ret = indices[1];
+  printf("Index Int : %u \n",index_int);
+  return index_int;
   // indices[1] should be the number of step points we have
-  printf(" evt 1 %.2f evt 2 %.2f evt 3 %.2f evt 4 %.2f evt 5 %.2f evt 6 %.2f \n ",evt[indices[2 + indices[0]]],evt[indices[2 + indices[0]]+1],evt[indices[2 + indices[0]]+2],evt[indices[2 + indices[0]]+3],evt[indices[2 + indices[0]]+4],evt[indices[2 + indices[0]]+5]);
-  for(unsigned int i = 0; i < indices[1]; i++)
+  //printf(" evt 1 %.2f evt 2 %.2f evt 3 %.2f evt 4 %.2f evt 5 %.2f evt 6 %.2f \n ",evt[indices[2 + indices[0]]],evt[indices[2 + indices[0]]+1],evt[indices[2 + indices[0]]+2],evt[indices[2 + indices[0]]+3],evt[indices[2 + indices[0]]+4],evt[indices[2 + indices[0]]+5]);
+
+  /*for(unsigned int i = 0; i < indices[1]; i++)
   {
     if(index_int < p[indices[2 + i]])
     {
-      ret = i;
+      ret = index_int;
       printf("\n ===== ===== ===== ===== ===== ===== RUNTIME ===== ===== ===== ===== ===== ===== ===== \n Input : x = %.3f (int = %u ) \n ===== INSIDE \n Threshold : %.2f \n Index returning %u \n",x,index_int,p[indices[2 + i]],ret);
       return ret;
     }
   }
   printf("\n ===== ===== ===== ===== ===== ===== OUTSIDE ===== ===== ===== ===== ===== ===== ===== \n Input : x = %.3f (int = %u ) \n ===== INSIDE \n \n Index returning %u \n",x,index_int,ret);
   return ret;
+  */
+
 }
 
 MEM_DEVICE device_function_ptr ptr_to_IndexSelector = device_IndexSelector;
