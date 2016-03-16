3793b1b060147931364ef138742ba00956938bf9
AdrianoDee <adriano.diflorio@ba.infn.it>
2016-03-16 21:48:17 +0100

    vector<Variable*>& b

--- PDFs/IndexPdf.hh
+++ PDFs/IndexPdf.hh
@@ -5,7 +5,7 @@
 
 class IndexPdf : public GooPdf {
 public:
-  IndexPdf (std::string n,vector<Variable*>& b, vector<GooPdf*>& t);
+  IndexPdf (std::string n,vector<Variable*>& vars,vector<Variable*>& b, vector<GooPdf*>& t);
   // Map function m must be custom written to correspond to order of function list t. 
   __host__ fptype normalise () const;
 private:
