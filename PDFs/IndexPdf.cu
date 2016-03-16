8dceb1abf6de4f0d1102aa0d32e7a59400184c3b
AdrianoDee <adriano.diflorio@ba.infn.it>
2016-03-16 23:37:44 +0100

    i

--- PDFs/IndexPdf.cu
+++ PDFs/IndexPdf.cu
@@ -11,7 +11,7 @@ EXEC_TARGET fptype device_Index (fptype* evt, fptype* p, unsigned int* indices) 
   unsigned int compareIndex = 1;
   int debug = 0;
 
-  printf(" %.2e = %.2e = %.2e = %.2e = %.2e = %.2e = %.2e = %.2e = %.2e = %.2e = %.2e = %.2e = %.2e = %.2e = %.2e = %.2e = %.2e = %.2e = %.2e = %.2e = %.2e = %.2e = %.2e = %.2e \n",i,p[0],p[1],p[2],p[3],p[4],p[5],p[6],p[7],p[8],p[9],p[10],p[11],p[12],p[13],p[14],p[15],p[16],p[17],p[18],p[19]);
+  printf(" %.2e = %.2e = %.2e = %.2e = %.2e = %.2e = %.2e = %.2e = %.2e = %.2e = %.2e = %.2e = %.2e = %.2e = %.2e = %.2e = %.2e = %.2e = %.2e = %.2e = %.2e = %.2e = %.2e = %.2e \n",p[0],p[1],p[2],p[3],p[4],p[5],p[6],p[7],p[8],p[9],p[10],p[11],p[12],p[13],p[14],p[15],p[16],p[17],p[18],p[19]);
 
   printf("here %d \n",debug);debug++;
   for (size_t i = 1; i <= indices[0]; i++) {
