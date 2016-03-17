0105b5db195af88667d24ef334bad33519548e53
AdrianoDee <adriano.diflorio@ba.infn.it>
2016-03-17 16:43:54 +0100

    evt[indices[2 + indices[0]]]

--- PDFs/IndexPdf.cu
+++ PDFs/IndexPdf.cu
@@ -16,7 +16,6 @@ EXEC_TARGET fptype device_Index (fptype* evt, fptype* p, unsigned int* indices) 
   unsigned int compareIndex = 0;
   //printf(" %.2e = %.2e = %.2e = %.2e = %.2e = %.2e = %.2e = %.2e = %.2e = %.2e = %.2e = %.2e = %.2e = %.2e = %.2e = %.2e = %.2e = %.2e = %.2e = %.2e = %.2e = %.2e = %.2e = %.2e \n",p[0],p[1],p[2],p[3],p[4],p[5],p[6],p[7],p[8],p[9],p[10],p[11],p[12],p[13],p[14],p[15],p[16],p[17],p[18],p[19]);
   //printf(" %u = %u = %u = %u = %u = %u = %u = %u = %u = %u = %u = %u = %u = %u = %u = %u = %u = %u = %u = %u \n %.2e = %.2e = %.2e = %.2e = %.2e = %.2e = %.2e = %.2e = %.2e = %.2e = %.2e = %.2e = %.2e = %.2e = %.2e = %.2e = %.2e = %.2e = %.2e = %.2e = %.2e = %.2e \n %.2f - %.2f - %.2f ",indices[0],indices[1],indices[2],indices[3],indices[4],indices[5],indices[6],indices[7],indices[8],indices[9],indices[10],indices[11],indices[12],indices[13],indices[14],indices[15],p[indices[1]],p[indices[2]],p[2],p[3],p[4],p[5],p[6],p[7],p[8],p[9],p[10],p[11],p[12],p[13],p[14],p[15],p[16],p[17],evt[indices[2 + indices[0]]],evt[indices[2 + indices[0]]+1],evt[indices[2 + indices[0]]+2]);
-  printf("indices[0] = %d - numObservables = %u - numIndex = %u index = %.2f",indices[0],numObservables,numIndex,index); 
   for (int i = 1; i <= numIndex; i++) {
     /* code */
     //printf("x = %.2f Parameter %d =  %.2f \n",x,i,p[indices[i]]);debug++;
@@ -25,11 +24,13 @@ EXEC_TARGET fptype device_Index (fptype* evt, fptype* p, unsigned int* indices) 
       //printf("Inside -> x = %.2f Parameter %d =  %.2f \n",x,i,p[indices[i]]);debug++;
     }
   }
+
   //printf("here %d \n",debug);debug++;
   //unsigned int mapFunction = (int) FLOOR(0.5 + x
   //This is an index into the IndexPdf's list of functions
   //int targetFunction = (int) FLOOR(0.5 + (*(reinterpret_cast<device_function_ptr>(device_function_table[mapFunction])))(evt, p, paramIndices + indices[2]));
   functionIndex = numIndex+2*(compareIndex-1)+1;
+  printf("indices[0] = %d - numObservables = %u - numIndex = %u index = %.2f z = %.2f x = %.2f y = %.2f compareIndex = %u functionIndex = %u\n",indices[0],numObservables,numIndex,index,evt[indices[2 + indices[0]]+1],evt[indices[2 + indices[0]]+2],evt[indices[2 + indices[0]]+3],compareIndex,functionIndex);
   //printf("here %d \n",debug);debug++;
   //Because there are two pieces of information about each function
   //Because first function information begins at index 3
