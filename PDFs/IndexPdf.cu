4c1d0abedafa5ce93a23d299918d27f0c87cb279
AdrianoDee <adriano.diflorio@ba.infn.it>
2016-03-17 15:40:20 +0100

    evt[indices[2 + indices[0]]+1]

--- PDFs/IndexPdf.cu
+++ PDFs/IndexPdf.cu
@@ -7,31 +7,31 @@ EXEC_TARGET fptype device_Index (fptype* evt, fptype* p, unsigned int* indices) 
   //printf("indices[0] = %.3f indices[1] = %.3f indices[2] = %.3f indices[2] = %.3f indices[3] = %.3f indices[4] = %.3f indices[5] = %.3f indices[6] = %.3f indices[7] = %.3f indices[8] = %.3f\n",indices[0],indices[1],indices[2],indices[3],indices[4],indices[5],indices[6],indices[7],indices[8]);
   //printf("p[0] = %.3f p[1] = %.3f p[2] = %.3f p[2] = %.3f p[3] = %.3f p[4] = %.3f p[5] = %.3f p[6] = %.3f p[7] = %.3f p[8] = %.3f\n",p[0],p[1],p[2],p[3],p[4],p[5],p[6],p[7],p[8]);
 
-  fptype x = evt[indices[2 + indices[0]]];
-  unsigned int compareIndex = 1;
-  int debug = 0;
-
+  unsigned int numParams = indices[0];
+  unsigned int numIndex = numParams/3;
+  fptype index = evt[indices[2 + indices[0]]];
+  std::vector <fptype> x;
+  unsigned int functionIndex = 1;
+  unsigned int compareIndex = 0;
   //printf(" %.2e = %.2e = %.2e = %.2e = %.2e = %.2e = %.2e = %.2e = %.2e = %.2e = %.2e = %.2e = %.2e = %.2e = %.2e = %.2e = %.2e = %.2e = %.2e = %.2e = %.2e = %.2e = %.2e = %.2e \n",p[0],p[1],p[2],p[3],p[4],p[5],p[6],p[7],p[8],p[9],p[10],p[11],p[12],p[13],p[14],p[15],p[16],p[17],p[18],p[19]);
-  printf(" %u = %u = %u = %u = %u = %u = %u = %u = %u = %u = %u = %u = %u = %u = %u = %u = %u = %u = %u = %u = %u = %u = %u = %u \n %.2e = %.2e = %.2e = %.2e = %.2e = %.2e = %.2e = %.2e = %.2e = %.2e = %.2e = %.2e = %.2e = %.2e = %.2e = %.2e = %.2e = %.2e = %.2e = %.2e = %.2e = %.2e = %.2e = %.2e \n",indices[0],indices[1],indices[2],indices[3],indices[4],indices[5],indices[6],indices[7],indices[8],indices[9],indices[10],indices[11],indices[12],indices[13],indices[14],indices[15],indices[16],indices[17],indices[18],indices[19],p[indices[1]],p[indices[2]],p[2],p[3],p[4],p[5],p[6],p[7],p[8],p[9],p[10],p[11],p[12],p[13],p[14],p[15],p[16],p[17],p[18],p[19]);
+  printf(" %u = %u = %u = %u = %u = %u = %u = %u = %u = %u = %u = %u = %u = %u = %u = %u = %u = %u = %u = %u = %u = %u = %u = %u \n %.2e = %.2e = %.2e = %.2e = %.2e = %.2e = %.2e = %.2e = %.2e = %.2e = %.2e = %.2e = %.2e = %.2e = %.2e = %.2e = %.2e = %.2e = %.2e = %.2e = %.2e = %.2e = %.2e = %.2e \n %.2f - %.2f - %.2f ",indices[0],indices[1],indices[2],indices[3],indices[4],indices[5],indices[6],indices[7],indices[8],indices[9],indices[10],indices[11],indices[12],indices[13],indices[14],indices[15],indices[16],indices[17],indices[18],indices[19],p[indices[1]],p[indices[2]],p[2],p[3],p[4],p[5],p[6],p[7],p[8],p[9],p[10],p[11],p[12],p[13],p[14],p[15],p[16],p[17],p[18],p[19],evt[indices[2 + indices[0]]],evt[indices[2 + indices[0]]+1],evt[indices[2 + indices[0]]+2]);
 
-  //printf("here %d \n",debug);debug++;
-  for (size_t i = 1; i <= indices[0]; i++) {
+  for (int i = 1; i <= numIndex; i++) {
     /* code */
-    //printf("x = %.2f Parameter %d =  %.2f \n",x,i,p[indices[i]]);debug++;
+    printf("x = %.2f Parameter %d =  %.2f \n",x,i,p[indices[i]]);debug++;
     if(p[indices[i]] == x){
       compareIndex = i;
       //printf("Inside -> x = %.2f Parameter %d =  %.2f \n",x,i,p[indices[i]]);debug++;
-      break;
     }
   }
   //printf("here %d \n",debug);debug++;
   //unsigned int mapFunction = (int) FLOOR(0.5 + x
     // This is an index into the IndexPdf's list of functions
   //int targetFunction = (int) FLOOR(0.5 + (*(reinterpret_cast<device_function_ptr>(device_function_table[mapFunction])))(evt, p, paramIndices + indices[2]));
-  int targetFunction = compareIndex;
+  targetFunction = numIndex+2*(compareIndex-1)+1;
   //printf("here %d \n",debug);debug++;
-  targetFunction *= 2; // Because there are two pieces of information about each function
-  targetFunction += indices[0]; // Because first function information begins at index 3
+ // Because there are two pieces of information about each function
+ // Because first function information begins at index 3
   //printf("here %d \n",debug);debug++;
   //fptype ret = (*(reinterpret_cast<device_function_ptr>(device_function_table[indices[targetFunction]])))(evt, p, paramIndices + indices[targetFunction + 1]);
   //fptype ret = callFunction(evt+compareIndex, indices[targetFunction], indices[targetFunction + 1]);
@@ -56,7 +56,8 @@ __host__ IndexPdf::IndexPdf (std::string n,vector<Variable*>& vars,vector<Variab
   std::vector<unsigned int> pindices;
   //pindices.push_back(m->getFunctionIndex());
   //pindices.push_back(m->getParameterIndex());
-  if (b.size() != t.size()) abortWithCudaPrintFlush(__FILE__, __LINE__, getName() + " Index Size =! Pdf Size", this);
+
+  assert((t.size() == b.size()));
 
   for (vector<Variable*>::iterator v = b.begin(); v != b.end(); ++v) {
     //components.push_back(*f);
