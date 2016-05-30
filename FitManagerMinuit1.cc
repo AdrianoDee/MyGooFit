17722e5fe86dc2787b934f9b35b7636c2664609e
AdrianoDee <adriano.diflorio@ba.infn.it>
2016-05-30 16:57:02 +0200

    >/dev/null

--- FitManagerMinuit1.cc
+++ FitManagerMinuit1.cc
@@ -2,79 +2,79 @@
 #include <omp.h>
 #endif
 
-PdfBase* pdfPointer; 
-FitManager* currGlue = 0; 
-int numPars = 0; 
-vector<Variable*> vars; 
+PdfBase* pdfPointer;
+FitManager* currGlue = 0;
+int numPars = 0;
+vector<Variable*> vars;
 
-void specialTddpPrint (double fun); 
+void specialTddpPrint (double fun);
 
-FitManager::FitManager (PdfBase* dat) 
+FitManager::FitManager (PdfBase* dat)
   : minuit(0)
   , overrideCallLimit(-1)
 {
   pdfPointer = dat;
-  currGlue = this; 
-} 
+  currGlue = this;
+}
 
 FitManager::~FitManager () {
-  if (minuit) delete minuit; 
+  if (minuit) delete minuit;
 }
 
 void FitManager::setupMinuit () {
-  vars.clear(); 
-  pdfPointer->getParameters(vars); 
+  vars.clear();
+  pdfPointer->getParameters(vars);
 
   numPars = vars.size();
   if (minuit) delete minuit;
-  minuit = new TMinuit(numPars); 
-  int maxIndex = 0; 
-  int counter = 0; 
+  minuit = new TMinuit(numPars);
+  int maxIndex = 0;
+  int counter = 0;
   for (std::vector<Variable*>::iterator i = vars.begin(); i != vars.end(); ++i) {
-    minuit->DefineParameter(counter, (*i)->name.c_str(), (*i)->value, (*i)->error, (*i)->lowerlimit, (*i)->upperlimit); 
+    minuit->DefineParameter(counter, (*i)->name.c_str(), (*i)->value, (*i)->error, (*i)->lowerlimit, (*i)->upperlimit);
     if ((*i)->fixed) minuit->FixParameter(counter);
-    counter++; 
+    counter++;
     if (maxIndex < (*i)->getIndex()) maxIndex = (*i)->getIndex();
   }
 
-  numPars = maxIndex+1; 
-  pdfPointer->copyParams();   
-  minuit->SetFCN(FitFun); 
-  //minuit->SetPrintLevel(-1);
+  numPars = maxIndex+1;
+  pdfPointer->copyParams();
+  minuit->SetFCN(FitFun);
+  minuit->SetPrintLevel(-1);
 }
 
 void FitManager::fit () {
-         
-   setupMinuit();  
+
+   setupMinuit();
    runMigrad();
- 
+
 }
 
-void FitManager::runMigrad () { 
+void FitManager::runMigrad () {
   assert(minuit);
   host_callnumber = 0;
   if (0 < overrideCallLimit) {
-    std::cout << "Calling MIGRAD with call limit " << overrideCallLimit << std::endl; 
+    std::cout << "Calling MIGRAD with call limit " << overrideCallLimit << std::endl;
     double plist[1];
     plist[0] = overrideCallLimit;
-    int err = 0; 
+    int err = 0;
     minuit->mnexcm("MIGRAD", plist, 1, err);
   }
   else{
-  
+
 //   double arglist[10];
 //   arglist [0]=2;
 //   int ierflg=0;
 //   minuit->mnexcm("SET STR",arglist,1,ierflg);
-   
-   minuit->Migrad(); 
-  
+
+   minuit->Migrad();
+
   }
- 
+
 }
 
 void FitManager::getMinuitValues () const {
-  int counter = 0; 
+  int counter = 0;
   for (std::vector<Variable*>::iterator i = vars.begin(); i != vars.end(); ++i) {
     minuit->GetParameter(counter++, (*i)->value, (*i)->error);
   }
@@ -82,59 +82,59 @@ void FitManager::getMinuitValues () const {
 
 void FitFun(int &npar, double *gin, double &fun, double *fp, int iflag) {
   vector<double> pars;
-  // Notice that npar is number of variable parameters, not total. 
+  // Notice that npar is number of variable parameters, not total.
   pars.resize(numPars);
-   
-  int counter = 0;
- 
-  for (std::vector<Variable*>::iterator i = vars.begin(); i != vars.end(); ++i) {
-    if (isnan(fp[counter])) cout << "Variable " << (*i)->name << " " << (*i)->index << " is NaN\n"; 
 
-    pars[(*i)->getIndex()] = fp[counter++] + (*i)->blind; 
- 
+  int counter = 0;
+
+  for (std::vector<Variable*>::iterator i = vars.begin(); i != vars.end(); ++i) {
+    if (isnan(fp[counter])) cout << "Variable " << (*i)->name << " " << (*i)->index << " is NaN\n";
+
+    pars[(*i)->getIndex()] = fp[counter++] + (*i)->blind;
+
  }
 
-  pdfPointer->copyParams(pars); 
+  pdfPointer->copyParams(pars);
 
-  fun = pdfPointer->calculateNLL(); 
+  fun = pdfPointer->calculateNLL();
 
-  host_callnumber++; 
+  host_callnumber++;
 
 #ifdef PRINTCALLS
-  specialTddpPrint(fun); 
-#endif 
+  specialTddpPrint(fun);
+#endif
 }
 
 #ifdef PRINTCALLS
 void specialTddpPrint (double fun) {
-  // Stupid amplitude-fit debugging method. 
+  // Stupid amplitude-fit debugging method.
   cout << "Function call " << host_callnumber << ": " << fun << "\n";
   currGlue->getMinuitValues();
-  int varCount = 1; 
+  int varCount = 1;
   for (std::vector<Variable*>::iterator v = vars.begin(); v != vars.end(); ++v) {
-    if (!(*v)) cout << "Null!" << endl; 
-    if ((*v)->fixed) continue; 
+    if (!(*v)) cout << "Null!" << endl;
+    if ((*v)->fixed) continue;
 
-    const fptype _mD0 = 1.86484; 
+    const fptype _mD0 = 1.86484;
     const fptype _mD02 = _mD0 *_mD0;
-    const fptype _mD02inv = 1./_mD02; 
-    double stupidSpecialModifier = 1; // Mikhail interprets some of the weights differently. 
-    if (((*v)->name == "f0_980_amp_real") || 
+    const fptype _mD02inv = 1./_mD02;
+    double stupidSpecialModifier = 1; // Mikhail interprets some of the weights differently.
+    if (((*v)->name == "f0_980_amp_real") ||
 	((*v)->name == "f0_980_amp_imag") ||
-	((*v)->name == "f0_1370_amp_real") || 
+	((*v)->name == "f0_1370_amp_real") ||
 	((*v)->name == "f0_1370_amp_imag") ||
-	((*v)->name == "f0_1500_amp_real") || 
+	((*v)->name == "f0_1500_amp_real") ||
 	((*v)->name == "f0_1500_amp_imag") ||
-	((*v)->name == "f0_1710_amp_real") || 
+	((*v)->name == "f0_1710_amp_real") ||
 	((*v)->name == "f0_1710_amp_imag") ||
-	((*v)->name == "f0_600_amp_real") || 
-	((*v)->name == "f0_600_amp_imag")) stupidSpecialModifier = -_mD02; 
+	((*v)->name == "f0_600_amp_real") ||
+	((*v)->name == "f0_600_amp_imag")) stupidSpecialModifier = -_mD02;
     else if (((*v)->name == "f2_1270_amp_real") ||
-	     ((*v)->name == "f2_1270_amp_imag")) stupidSpecialModifier = -_mD02inv; 
+	     ((*v)->name == "f2_1270_amp_imag")) stupidSpecialModifier = -_mD02inv;
     else if (((*v)->name == "nonr_amp_real") ||
-	     ((*v)->name == "nonr_amp_imag")) stupidSpecialModifier = -1; 
+	     ((*v)->name == "nonr_amp_imag")) stupidSpecialModifier = -1;
 
-    cout.width(20); 
+    cout.width(20);
     cout << (*v)->name;
     cout.setf(ios_base::right,ios_base::adjustfield);
     cout.width(3);
@@ -145,9 +145,9 @@ void specialTddpPrint (double fun) {
     cout.setf(ios_base::right,ios_base::adjustfield); cout.precision(8);
     cout << "  ";         cout.width(12);
     cout << (*v)->error;
-    cout << endl; 
+    cout << endl;
   }
 
-  cout << endl; 
+  cout << endl;
 }
 #endif
