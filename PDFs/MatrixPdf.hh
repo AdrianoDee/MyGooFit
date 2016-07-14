272aa19c015ee0634393bd1e7962829bf9798229
AdrianoDee <adriano.diflorio@ba.infn.it>
2016-07-14 16:24:18 +0200

    /**/

--- PDFs/MatrixPdf.hh
+++ PDFs/MatrixPdf.hh
@@ -3,7 +3,7 @@
 
 #include "TComplex.h"
 #include "TMath.h"
-
+/*
 #define ZERO 0
 #define M1 1
 #define P1 2
@@ -28,15 +28,16 @@
 #define K1430_2 14302
 #define H1430_2_0 143020
 #define H1430_2_p1 143020
-#define H1430_2_m1 143020
+#define H1430_2_m1 143020*/
 
+/*
 MEM_CONSTANT fptype MLb = 5.61951;
 MEM_CONSTANT fptype MBd = 5.27961;
 MEM_CONSTANT fptype MPsi2S = 3.686109;
 MEM_CONSTANT fptype MJpsi = 3.096916;
 MEM_CONSTANT fptype MProton = 0.938272046;
 MEM_CONSTANT fptype MKaon = 0.493677;
-MEM_CONSTANT fptype MPion = 0.13957018;
+MEM_CONSTANT fptype MPion = 0.13957018;*/
 
 /*
 MEM_CONSTANT fptype MLb2 = MLb*MLb;
@@ -69,13 +70,13 @@ MEM_CONSTANT fptype TwoMKaon2pTwoMPion2 = 2.*(MKaon2 + MPion2);*/
 
 
 // K*
-MEM_CONSTANT fptype M892 = 0.89581 ; MEM_CONSTANT fptype G892 = 0.0474; // From PDG charged only K*(892)
-MEM_CONSTANT fptype M1410 = 1.414; MEM_CONSTANT fptype G1410 = 0.232; // K*1410
-MEM_CONSTANT fptype M1430 = 1.425; MEM_CONSTANT fptype G1430 = 0.270; // K*1430
-MEM_CONSTANT fptype M800 = 0.931; MEM_CONSTANT fptype G800 = 0.578; // K*800
-MEM_CONSTANT fptype M1430_2 = 1.4324; MEM_CONSTANT fptype G1430_2 = 0.109; // K*1430_2
+const fptype M892 = 0.89581 ; const fptype G892 = 0.0474; // From PDG charged only K*(892)
+const fptype M1410 = 1.414; const fptype G1410 = 0.232; // K*1410
+const fptype M1430 = 1.425; const fptype G1430 = 0.270; // K*1430
+const fptype M800 = 0.931; const fptype G800 = 0.578; // K*800
+const fptype M1430_2 = 1.4324; const fptype G1430_2 = 0.109; // K*1430_2
 
-MEM_CONSTANT fptype K892_1 = 892.1;
+const fptype K892_1 = 892.1;
 
 
 class MatrixPdf : public GooPdf {
