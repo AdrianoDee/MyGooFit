fcf9dfc5a54fea0406e89ded161ee7fd9816e452
AdrianoDee <adriano.diflorio@ba.infn.it>
2016-10-23 15:47:38 +0200

    ;

--- PDFs/GooPdf.cu
+++ PDFs/GooPdf.cu
@@ -742,7 +742,7 @@ EXEC_TARGET fptype MetricTaker::operator () (thrust::tuple<fptype*,int, int> t) 
   // the structure of the event is (obs1 obs2... binentry binvolume), so that the array
   // passed to the metric consists of (binentry binvolume).
   ret = (*(reinterpret_cast<device_metric_ptr>(device_function_table[metricIndex])))(ret, eventAddress + (abs(eventSize)-2), parameters);
-  printf("Metrica : %.3f \n",ret)
+  printf("Metrica : %.3f \n",ret);
   return ret;
 }
 
