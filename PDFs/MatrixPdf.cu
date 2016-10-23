efe53fc23554f7d3b0b14160a4577bdd050302a2
AdrianoDee <adriano.diflorio@ba.infn.it>
2016-10-23 17:35:40 +0200

    Ciao

--- PDFs/MatrixPdf.cu
+++ PDFs/MatrixPdf.cu
@@ -454,12 +454,12 @@ EXEC_TARGET fptype device_Matrix (fptype* evt, fptype* p, unsigned int* indices)
 
     fptype cKs = cosTheta_FromMasses(mKP2, mPsiP2, MPsi_nS2, MBd2, MKaon2, MPion2);
 
-  if (FABS(cKs) > 1) return 0.;
+  if (FABS(cKs) > 1) { printf("ckS > 1 or < -1 ") return 0.};
 
     //printf("Hei mpk = %.2f cJ = %.2f cKs = %.2f phi = %.2f psi_nS = %.2f dRadB0 = %.2f dRadKs = %.2f mPSi = %.2f \n",mkp,cJ,cKs,phi,psi_nS,dRadB0,dRadKs,mPsiP);
 
   if ((mkp < MKaon + MPion) || (mkp > MBd - MPsi_nS) || (mPsiP < MPsi_nS + MPion) || (mPsiP > MBd - MKaon)){
-    //printf("Out of the borders \n");
+    printf("Out of the borders \n");
     return 0.;}
   else{
       fptype MEME = ME2(mkp,cJ,cKs,phi,p,indices);
