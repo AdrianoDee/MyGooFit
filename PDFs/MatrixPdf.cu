d2d7ab18197c1881895364eb0792e6ed6a5e8adf
AdrianoDee <adriano.diflorio@ba.infn.it>
2016-10-31 16:56:49 +0100

    Counting a AND b

--- PDFs/MatrixPdf.cu
+++ PDFs/MatrixPdf.cu
@@ -139,8 +139,8 @@ EXEC_TARGET devcomplex<fptype> H(fptype* p,unsigned int* indices, fptype helJ,in
   // 2      3      4      5  6  7  8  9  10        4+n 5+n 6+n  4+nKstars*3 4+nKstars*3+1              4+nKstars*3+(n-1)*2 4+nKstars*3+(n-1)*2+1
 
   int noOfMasses = indices[1];
-  fptype a = p[indices[5+noOfMasses*3+lA+whichOfThree*2]];
-  fptype b = p[indices[5+noOfMasses*3+lA+whichOfThree*2+1]];
+  fptype a = p[indices[5+noOfMasses*3+lA*2+whichOfThree*2]];
+  fptype b = p[indices[5+noOfMasses*3+lA*2+whichOfThree*2+1]];
 
   #ifdef MDEBUGGING
   if(helJ==ZEROHEL) printf("Which of Three : %d Index : %d  a = %.3f  b = %.3f for helJ = 0 (%.2f) noOfMasses = %d \n",whichOfThree,5+noOfMasses*3+lA+whichOfThree*2,a,b,helJ,noOfMasses);
