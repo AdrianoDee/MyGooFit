f4627d95d68d918c150337705d11a27ffa201ecd
AdrianoDee <adriano.diflorio@ba.infn.it>
2016-11-28 16:31:02 +0100

    Testing, debugging

--- PDFs/InterHistPdf.hh
+++ PDFs/InterHistPdf.hh
@@ -1,22 +1,22 @@
 #ifndef INTERHIST_PDF_HH
 #define INTERHIST_PDF_HH
 
-#include "GooPdf.hh" 
-#include "BinnedDataSet.hh" 
+#include "GooPdf.hh"
+#include "BinnedDataSet.hh"
 
 class InterHistPdf : public GooPdf {
 public:
-  InterHistPdf (std::string n, 
-			  BinnedDataSet* x, 
-			  std::vector<Variable*> params, 
-			  std::vector<Variable*> obses); 
+  InterHistPdf (std::string n,
+			  BinnedDataSet* x,
+			  std::vector<Variable*> params,
+			  std::vector<Variable*> obses);
   //__host__ virtual fptype normalise () const;
 
 private:
-  thrust::device_vector<fptype>* dev_base_histogram; 
-  fptype totalEvents; 
+  thrust::device_vector<fptype>* dev_base_histogram;
+  fptype totalEvents;
   fptype* host_constants;
-  int numVars; 
+  int numVars;
 };
 
 #endif
