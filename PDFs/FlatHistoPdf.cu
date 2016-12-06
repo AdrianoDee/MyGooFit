6635c0dad8b0a24fcf9c17d54d4a063180c5ca59
AdrianoDee <adriano.diflorio@ba.infn.it>
2016-12-06 19:48:06 +0100

    Hopp

--- PDFs/FlatHistoPdf.cu
+++ PDFs/FlatHistoPdf.cu
@@ -48,7 +48,7 @@ EXEC_TARGET fptype device_FlatHistogram (fptype* evt, fptype* p, unsigned int* i
     globalBin      += previous * localBin;
     previous       *= indices[lowerBoundIdx + 2];
 
-    printf("Curr Variable %d = %.2f (%.2f %.2f %.2f %.2f)numVars %d globalBin %d localBin %f ",i,currVariable,evt[indices[indices[0] + 2]],evt[indices[indices[0] + 2 + 1]],evt[indices[indices[0] + 2 + 2]],evt[indices[indices[0] + 2 + 3]],numVars,globalBin,localBin);
+    printf("Curr Variable %d = %.2f (%.2f %.2f %.2f %.2f)numVars %d globalBin %d localBin %f \n",i,currVariable,evt[indices[indices[0] + 2]],evt[indices[indices[0] + 2 + 1]],evt[indices[indices[0] + 2 + 2]],evt[indices[indices[0] + 2 + 3]],numVars,globalBin,localBin);
 
   }
 
