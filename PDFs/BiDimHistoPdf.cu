32a2665c11ad7c01210844cfba7882cc1018aedc
AdrianoDee <adriano.diflorio@ba.infn.it>
2016-12-27 17:17:34 +0100

    yarr[index-fbinLo]

--- PDFs/BiDimHistoPdf.cu
+++ PDFs/BiDimHistoPdf.cu
@@ -91,7 +91,7 @@ MEM_CONSTANT fptype* dev_base_bidimhisto[100]; // Multiple histograms for the ca
          ibin = index;
          xarr[index-fbinLo] = lowerBound+ibin*step-step*0.5;
          yarr[index-fbinLo] = histogram[ibin];
-         printf("Bin histo pdf 2 = %.3f %d %d %d %d %.3f %.3f \n",xval,localBin,index,ibin,localNumBins,xarr[index-fbinLo],histogram[ibin]);
+         printf("Bin histo pdf 2 = %.3f %d %d %d %d %.3f %.3f %.3f\n",xval,localBin,index,ibin,localNumBins,xarr[index-fbinLo],histogram[ibin],yarr[index-fbinLo]);
        } else if (index>=localNumBins) {
         //  ibin = 2*localNumBins-index-1 ;
          printf("Over binning 2 \n");
