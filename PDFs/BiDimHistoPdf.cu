9d0748fd7b86ef6fb2fcef7027121c3b51960a3f
AdrianoDee <adriano.diflorio@ba.infn.it>
2016-12-27 16:22:13 +0100

    Debugging

--- PDFs/BiDimHistoPdf.cu
+++ PDFs/BiDimHistoPdf.cu
@@ -107,9 +107,66 @@ MEM_CONSTANT fptype* dev_base_bidimhisto[100]; // Multiple histograms for the ca
 
      printf("Bin histo pdf 2.1 = %.3f %d %d \n",xval,localBin,localNumBins);
 
-     fptype ret = interArrays(x,xarr,yarr,intOrder+1);
+     //fptype ret = interArrays(x,xarr,yarr,intOrder+1);
 
-     return ret;
+     fptype xvalue = xval;
+
+     printf("Bin histo pdf 3.0 = %.3f %d \n",xvalue,intOrder);
+
+
+     fptype den,dif,dift,ho,hp,w,y,dy;
+     fptype coeffC[20], coeffD[20];
+
+     dif = fabs(xvalue-xArray[0]) ;
+
+     int ns=1;
+
+     for(int intexInter =1 ; intexInter<=intOrder+1 ; ++intexInter)
+     {
+       dift=fabs(xvalue-xArray[intexInter-1]);
+       if (dift<dif)
+       {
+          ns = intexInter;
+          dif = dift ;
+       }
+
+       coeffC[intexInter] = yArray[intexInter-1];
+       coeffD[intexInter] = yArray[intexInter-1];
+
+       printf("Bin histo pdf 3 = %.3f %d %.3f %.3f %.3f %.3f \n",xvalue,intexInter,dift,dif,xArray[intexInter-1],coeffC[intexInter],coeffD[intexInter]);
+
+
+     }
+
+     y=yArray[--ns] ;
+
+     for(int m=1 ; m<intOrder+1; m++)
+     {
+       for(int intexInter=1 ; intexInter<=intOrder+1-m ; intexInter++)
+       {
+         ho=xArray[intexInter-1]-xvalue ;
+         hp=xArray[intexInter-1+m]-xvalue ;
+         w=coeffC[intexInter+1]-coeffD[intexInter] ;
+         den=ho-hp ;
+         if (den==0.)
+         {
+           return 0. ;
+         }
+         den = w/den ;
+         coeffD[intexInter]=hp*den ;
+         coeffC[intexInter]=ho*den;
+         }
+         dy = (2*ns)<(intOrder+1-m) ? coeffC[ns+1] : coeffD[ns--] ;
+         y += dy ;
+
+         printf("Bin histo pdf 4 = %.3f %.3f %.3f %.3f %.3f %.3f\n",xvalue,ho,hp,w,den,dy);
+
+
+       }
+
+       return y;
+
+     //return ret;
 
    }
 
